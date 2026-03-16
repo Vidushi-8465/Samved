b646938155643fe8b2d07f6ed638bcc422236d19
blacktornado2 <blacktornado2108@gmail.com>
2026-03-16 15:56:02 +0530

    fix: rename file and add assets dir.

--- services/firebase.ex.ts
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCUjpfnoTOSA1UugTBCtkhrB2a8f4h3vWA",
-  authDomain: "surakshanet-smc.firebaseapp.com",
-  databaseURL: "https://surakshanet-smc-default-rtdb.firebaseio.com",
-  projectId: "surakshanet-smc",
-  storageBucket: "surakshanet-smc.firebasestorage.app",
-  messagingSenderId: "467586782312",
-  appId: "1:467586782312:web:33b81f93b60cc61c9d57dd"
-};
